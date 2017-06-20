b0f2e99bbded8372dc382ee54c25020d7b7d20e8
Zamplin <zamplin@gmail.com>
2017-06-21 00:06:49 +0200

    mon premier commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyglet
+
+window = pyglet.window.Window()
+image = pyglet.resource.image('img/kitten.jpg')
+image.height
+
+
+@window.event
+def on_draw():
+    window.clear()
+    image.blit(0, 0)
+
+pyglet.app.run()
